a0471e792b05b1b76a0c34ad471ac65c889861d4
Aleksander Kwiatkowski <bobikx@poczta.fm>
2014-01-26 18:14:31 +0100

    detail - not refreshed data

--- spec/put_sth_into_redis.rb
+++ spec/put_sth_into_redis.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe HelloServerClient do
-  it "put something into redis" do
+  it "put something into redis and get it back" do
     s = HelloServerClient::Service.find_or_initialize_by_name("temp")
     s.value = {
-      test: "ok",
-      time: Time.now
+      "test" => "ok",
+      "time" => Time.now
     }
-    s.save
+    s.detail = {
+      "detail" => "ok"
+    }
+    s.save!
+
+    a = HelloServerClient::Service.find_or_initialize_by_name("temp")
+    s.value["test"].should == "ok"
+    s.detail["detail"].should == "ok"
   end
 end
